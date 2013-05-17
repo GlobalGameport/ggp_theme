4e2eb6a99cdf26bd91b8de55800642bfcab4ce82
valkum <rudi.floren@googlemail.com>
2013-05-17 03:49:14 +0200

    removed pubdate from time elements

--- template.php
+++ template.php
@@ -72,8 +72,14 @@ function ggp_theme_process_page(&$vars) {
 /**
  * Override or insert variables into the node templates.
  */
-/* -- Delete this line if you want to use these functions
+
 function ggp_theme_preprocess_node(&$vars) {
+  // Publication date, formatted with time element
+  $vars['publication_date'] = '<time datetime="' . $vars['datetime'] . '">' . $vars['date'] . '</time>';
+   // Last updated formatted in time element
+  $vars['last_update'] = '<time datetime="' . $vars['datetime_updated'] . '">' . $vars['custom_date_and_time'] . '</time>';
+
+
 }
 
 function ggp_theme_process_node(&$vars) {
