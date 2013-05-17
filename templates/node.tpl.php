8c72a058a5c85f268a4723d253c8a9b7cc2beefd
valkum <rudi.floren@googlemail.com>
2013-05-17 03:34:28 +0200

    update to adaptivetheme 7.x-3.1 and some html validation fixes

--- templates/node.tpl.php
+++ templates/node.tpl.php
@@ -1,10 +1,14 @@
-<article id="article-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
-  <?php print $unpublished; ?>
+<?php
+    hide($content['comments']);
+    hide($content['links']);
+?>
+<article id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
 
   <?php print render($title_prefix); ?>
+
   <?php if ($title && !$page): ?>
-    <header>
+    <header<?php print $header_attributes; ?>>
       <?php if ($title): ?>
         <h2<?php print $title_attributes; ?>>
           <a href="<?php print $node_url; ?>" rel="bookmark"><?php print $title; ?></a>
@@ -15,24 +19,18 @@
   <?php print render($title_suffix); ?>
 
   <?php if ($display_submitted): ?>
-    <footer class="submitted<?php $user_picture ? print ' with-user-picture' : ''; ?>">
-      <?php print $user_picture; ?>
+    <footer<?php print $footer_attributes; ?>>
       <p class="author-datetime"><?php print $submitted; ?></p>
     </footer>
   <?php endif; ?>
 
   <div<?php print $content_attributes; ?>>
-  <?php
-    hide($content['comments']);
-    hide($content['links']);
-    print render($content);
-  ?>
+  <?php print render($content); ?>
   </div>
 
   <?php if ($links = render($content['links'])): ?>
-    <nav class="clearfix"><?php print $links; ?></nav>
+    <nav<?php print $links_attributes; ?>><?php print $links; ?></nav>
   <?php endif; ?>
 
-  <?php print render($content['comments']); ?>
 
 </article>
--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -2,7 +2,8 @@
 <div id="lines_top">
 
 
-<div id="page" class="container">
+<div id="page" class="container <?php print $classes; ?>">
+  <!-- region: Leaderboard -->
   <?php print render($page['leaderboard']); ?>
 
   <header class="clearfix" role="banner">
@@ -11,81 +12,73 @@
       <div id="logo"><?php print $linked_site_logo; ?></div>
     <?php endif; ?>
 
-    <?php if ($site_name || $site_slogan): ?>
-      <hgroup<?php if (!$site_slogan && $hide_site_name): ?> class="<?php print $visibility; ?>"<?php endif; ?>>
-        <?php if ($site_name): ?>
-          <div id="site-name"<?php if ($hide_site_name): ?> class="<?php print $visibility; ?>"<?php endif; ?>><?php print $site_name; ?></div>
-        <?php endif; ?>
-        <?php if ($site_slogan): ?>
-          <h2 id="site-slogan"><?php print $site_slogan; ?></h2>
-        <?php endif; ?>
-      </hgroup>
-    <?php endif; ?>
-
+    <!-- region: Header -->
     <?php print render($page['header']); ?>
 
   </header>
 
-  <?php //print render($page['menu_bar']); ?>
-  <?php //if ($primary_navigation): print $primary_navigation; endif; ?>
-  <?php //if ($secondary_navigation): print $secondary_navigation; endif; ?>
-
-
+  <!-- Messages and Help -->
   <?php print $messages; ?>
   <?php print render($page['help']); ?>
 
   <?php print render($page['secondary_content']); ?>
 
-  <div id="columns"><div class="columns-inner clearfix">
-    <div id="content-column"><div class="content-inner">
+  <div id="columns">
+    <div class="columns-inner clearfix">
+      <div id="content-column">
+        <div class="content-inner">
 
-      <?php if ($breadcrumb): ?>
-        <nav id="breadcrumb"><?php print $breadcrumb; ?></nav>
-      <?php endif; ?>
-
-      <?php print render($page['highlighted']); ?>
-
-      <?php $tag = $title ? 'section' : 'div'; ?>
-      <<?php print $tag; ?> id="main-content" role="main">
-
-        <?php print render($title_prefix); ?>
-        <?php if ($title || $primary_local_tasks || $secondary_local_tasks || $action_links = render($action_links)): ?>
-          <header>
-            <?php if (!$is_front && $title): ?>
-              <h1 id="page-title"><?php print $title; ?></h1>
-            <?php endif; ?>
-
-            <?php if ($primary_local_tasks || $secondary_local_tasks || $action_links): ?>
-              <div id="tasks">
-                <?php if ($primary_local_tasks): ?>
-                  <ul class="tabs primary clearfix"><?php print render($primary_local_tasks); ?></ul>
-                <?php endif; ?>
-                <?php if ($secondary_local_tasks): ?>
-                  <ul class="tabs secondary clearfix"><?php print render($secondary_local_tasks); ?></ul>
-                <?php endif; ?>
-                <?php if ($action_links = render($action_links)): ?>
-                  <ul class="action-links clearfix"><?php print $action_links; ?></ul>
-                <?php endif; ?>
-              </div>
-            <?php endif; ?>
-          </header>
+        <?php if ($breadcrumb): ?>
+          <nav id="breadcrumb"><?php print $breadcrumb; ?></nav>
         <?php endif; ?>
-        <?php print render($title_suffix); ?>
 
-        <div id="content"><?php print render($page['content']); ?></div>
+        <?php print render($page['highlighted']); ?>
 
-        <!--<?php print $feed_icons; ?>-->
+        <<?php print $tag; ?> id="main-content" role="main">
 
-      </<?php print $tag; ?>>
+          <?php if ($title || $primary_local_tasks || $secondary_local_tasks || $action_links = render($action_links)): ?>
+            <header id="main-content-header" class="clearfix">
 
-      <?php print render($page['content_aside']); ?>
+              <?php if ($title): ?>
+                <h1 id="page-title"><?php print $title; ?></h1>
+              <?php endif; ?>
 
-    </div></div>
+              <?php if ($primary_local_tasks || $secondary_local_tasks || $action_links): ?>
+                <div id="tasks" class="clearfix">
+                  <?php if ($primary_local_tasks): ?>
+                    <ul class="tabs primary clearfix"><?php print render($primary_local_tasks); ?></ul>
+                  <?php endif; ?>
+                  <?php if ($secondary_local_tasks): ?>
+                    <ul class="tabs secondary clearfix"><?php print render($secondary_local_tasks); ?></ul>
+                  <?php endif; ?>
+                  <?php if ($action_links = render($action_links)): ?>
+                    <ul class="action-links clearfix"><?php print $action_links; ?></ul>
+                  <?php endif; ?>
+                </div>
+              <?php endif; ?>
+            </header>
+          <?php endif; ?>
+          <?php print render($title_suffix); ?>
+
+          <?php if ($content = render($page['content'])): ?>
+            <div id="content">
+              <?php print $content; // Main content region ?>
+            </div>
+          <?php endif; ?>
+
+
+        </<?php print $tag; ?>> <!-- /main-content -->
+
+        <?php print render($page['content_aside']); ?>
+
+      </div>
+    </div> <!-- /content-column -->
 
     <?php print render($page['sidebar_first']); ?>
     <?php print render($page['sidebar_second']); ?>
 
-  </div></div>
+  </div>
+</div> <!-- /columns -->
 
   <?php print render($page['tertiary_content']); ?>
 
@@ -98,15 +91,17 @@
   <?php endif; ?>
 
   <?php if ($page['footer']): ?>
-    <footer role="contentinfo">
-      <div class="copyright">
-        &copy; 2006 - <?php echo date("Y"); ?> GlobalGameport.com - Alle Rechte vorbehalten  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Kopieren oder direktes Verlinken von Inhalten dieser Seite ohne schriftliche Erlaubnis des verantwortlichen Webmasters/Autors ist untersagt.<br /><a href="<?php echo base_path(); ?>impressum">Impressum</a> &nbsp;|&nbsp; <a href="<?php echo base_path(); ?>disclaimer">Disclaimer</a> &nbsp;|&nbsp; <a href="<?php echo base_path(); ?>sitemap.xml">Sitemap</a><br /><a href="<?php echo base_path(); ?>ueber-global-gameport">&Uuml;ber Global Gameport</a> &nbsp;|&nbsp; <a target="_blank" href="http://jobs.globalgameport.com">Jobs</a> &nbsp;|&nbsp; <a target"_blank" href="http://www.globalgameport.com">Forum</a> &nbsp;|&nbsp; <a target"_blank" href="http://www.globalgameport.com/register.php">Bei GGP registrieren</a> &nbsp;|&nbsp; <a target"_blank" href="http://www.facebook.com/globalgameport">GGP @ Facebook</a> &nbsp;|&nbsp; <a target"_blank" href="http://twitter.com/globalgameport">GGP @ Twitter</a> &nbsp;|&nbsp; <a target"_blank" href="http://www.youtube.com/user/globalgameport">GGP @ You Tube</a>
-      <br /><?php print render($page['footer']); ?>
-	  </div>
+    <footer id="footer" role="contentinfo">
+      <div id="footer-inner" class="clearfix">
+        <div class="copyright">
+          &copy; 2006 - <?php echo date("Y"); ?> GlobalGameport.com - Alle Rechte vorbehalten  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Kopieren oder direktes Verlinken von Inhalten dieser Seite ohne schriftliche Erlaubnis des verantwortlichen Webmasters/Autors ist untersagt.<br /><a href="<?php echo base_path(); ?>impressum">Impressum</a> &nbsp;|&nbsp; <a href="<?php echo base_path(); ?>disclaimer">Disclaimer</a> &nbsp;|&nbsp; <a href="<?php echo base_path(); ?>sitemap.xml">Sitemap</a><br /><a href="<?php echo base_path(); ?>ueber-global-gameport">&Uuml;ber Global Gameport</a> &nbsp;|&nbsp; <a href="http://jobs.globalgameport.com" target="_blank">Jobs</a> &nbsp;|&nbsp; <a href="http://www.globalgameport.com" target"_blank">Forum</a> &nbsp;|&nbsp; <a href="http://www.globalgameport.com/register.php" target"_blank">Bei GGP registrieren</a> &nbsp;|&nbsp; <a href="http://www.facebook.com/globalgameport" target"_blank">GGP @ Facebook</a> &nbsp;|&nbsp; <a href="http://twitter.com/globalgameport" target"_blank">GGP @ Twitter</a> &nbsp;|&nbsp; <a href="http://www.youtube.com/user/globalgameport" target"_blank">GGP @ You Tube</a>
+        <br /><?php print render($page['footer']); ?>
+    	  </div>
+      </div>
     </footer>  
   <?php endif; ?>
 
 </div>
 
-</div>
-</div>
+</div> <!-- /lines_top -->
+</div> <!-- /incredible_bg -->
