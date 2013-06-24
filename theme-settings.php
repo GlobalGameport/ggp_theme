c8179049c4fa20dc7be20107c7de05911c001876
valkum <rudi.floren@googlemail.com>
2013-06-24 23:51:20 +0200

    changed form type to textarea

--- theme-settings.php
+++ theme-settings.php
@@ -41,7 +41,7 @@ function ggp_theme_form_system_theme_settings_alter(&$form, &$form_state)  {
   );
 
   $form['at-settings']['ggp_customization']['custom_css'] = array(
-    '#type'=> 'textfield',
+    '#type'=> 'textarea',
     '#title' => t('Custom CSS'),
     '#description' => t('Set custom CSS which will be inclueded in head'),
 
